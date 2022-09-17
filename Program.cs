26aca7b15009b25e30d9baa48a06d17d857783c9
신정현 <@DESKTOP-J2VQN66>
2022-09-18 07:46:32 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nNum = 0;
+
+            while (true)
+            {
+                Console.WriteLine("nNum : {0}", nNum++);
+
+                if (nNum >= 5)
+                    break;
+            }
+        }
+    }
+}
